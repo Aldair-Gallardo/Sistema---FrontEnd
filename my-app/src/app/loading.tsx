c7b9ec8e3068ce06bd8043769e8fa4c67012b130
Aldair-Gallardo <aldairgallardo0903@gmail.com>
2026-07-11 23:17:13 -0500

    Ajustes a la estructura del FrontEnd del proyecto, mas adaptado a lo que haremos

---
+++ my-app/src/app/loading.tsx
@@ -0,0 +1,8 @@
+// aqui debe ir lo que aparece en pantalla cuando algo está cargando
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <p>Cargando...</p>
+    </div>
+  );
+}
